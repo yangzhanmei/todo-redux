f06b80f12acf768c2a253b837cbbcd43de4b2c7f
fat-gril <15809244435@163.com>
2016-08-21 17:43:31 +0800

    change if to switch in reducer

--- public/reducer/reducer.js
+++ public/reducer/reducer.js
@@ -1,19 +1,17 @@
-export default function reducer(state = {todos: [],filterName:'ALL'}, action) {
-    if (action.type === 'ADD') {
-        state.todos.push({todo: action.todo, isDone: false});
-        return state;
-    }
-    if (action.type === 'DELETE') {
-        state.todos.splice(action.index, 1);
-        return state;
-    }
-    if (action.type === 'TOGGLE') {
-        state.todos[action.index].isDone = !state.todos[action.index].isDone;
-        return state;
-    }
-    if(action.type === 'FILTER'){
-        state.filterName = action.filterName;
-        return state;
+export default function reducer(state = {todos: [], filterName: 'ALL'}, action) {
+    switch (action.type) {
+        case 'ADD':
+            state.todos.push({todo: action.todo, isDone: false});
+            return state;
+        case 'DELETE':
+            state.todos.splice(action.index, 1);
+            return state;
+        case 'TOGGLE':
+            state.todos[action.index].isDone = !state.todos[action.index].isDone;
+            return state;
+        case 'FILTER':
+            state.filterName = action.filterName;
+            return state;
     }
 
     return state;
